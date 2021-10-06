f8cef949eed04dff98f2517c366ed2abef4277e1
adityachandrafb <90758821+adityachandrafb@users.noreply.github.com>
2021-10-07 05:32:42 +0700

    update timer

--- model/timer.go
+++ model/timer.go
@@ -1,10 +1,27 @@
 package model
 
 import (
-	// "time"
-	// "gopkg.in/guregu/null.v4"
+	"time"
+	"gopkg.in/guregu/null.v4"
 	"gorm.io/gorm"
 )
+
+const (
+	Duration = 25 * time.Minute
+	Shortbreak = 5 * time.Minute
+	Longbreak = 15 * time.Minute
+	LongbreakInterval = 4 
+)
+
+
+type Timer struct {
+	gorm.Model
+	ID				uint   		`gorm:"foreignkey"`
+	Duration    time.Duration	`json:"duration"`
+	StartAt     time.Time		`json:"start_at"`
+	StopAt      null.Time		`json:"stop_at"`
+	Status    	*Respon			`json:"status"`
+}
 
 type Respon struct {
 	gorm.Model
